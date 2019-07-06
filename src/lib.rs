0969952b224186bca5d19706ce4e02b61f1245a0
Joshua Crowgey <jcrowgey@uw.edu>
2019-07-05 23:03:19 -0700

    Added triple-fault protection by handling all double faults

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,10 @@ use core::panic::PanicInfo;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
+pub mod gdt;
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
 
--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,6 @@ pub extern "C" fn _start() -> ! {
 
     yesos::init();
 
-    x86_64::instructions::interrupts::int3();
-
     #[cfg(test)]
     test_main();
 
